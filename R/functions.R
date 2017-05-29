a9f83ccce78ae5d2ba64334e99705fc86504609b
Cillian <cillian.miltown@gmail.com>
2017-05-29 16:26:07 +0100

    added function "p_report"

--- R/functions.R
+++ R/functions.R
@@ -269,6 +269,18 @@ df_aov <- function(a){
 
 }
 
+p_report <- function(b){
+  c <- print(formatC(b, digits = 3))
+  d <- length(strsplit(as.character(c), "")[[1]])
+  e <- round(b, digits = 3)
+  if (b < 0.001)
+    print(paste0("<", " ", ".001"))
+  else if (d > 4)
+    print(paste0("=", " ", e))
+  else print(paste0("=", " ", c))
+
+}
+
 toto <- function(){
   print("I hope this works")
 
